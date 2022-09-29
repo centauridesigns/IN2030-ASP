d4ad3c94f44aee93e25ad99c7f98228825d4106b
murvold <madsmurvold@gmail.com>
2022-09-29 21:14:12 +0200

    Tweaks prettyPrint

--- asp-interpreter/no/uio/ifi/asp/parser/AspFloatLiteral.java
+++ asp-interpreter/no/uio/ifi/asp/parser/AspFloatLiteral.java
@@ -24,7 +24,7 @@ public class AspFloatLiteral extends AspAtom {
     @Override
     public void prettyPrint() {
         String doubleStreng = String.valueOf(floaten);
-	    prettyWrite(" " + doubleStreng + " ");
+	    prettyWrite(doubleStreng + " ");
     }
 
 
